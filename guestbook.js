38aac668f7e0bf8cea333e074fe4454e73cc6d9f
mhmd-ym <102509138+mhmd-ym@users.noreply.github.com>
2022-06-11 19:12:05 +0430

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7d04a453.us-south.apigw.appdomain.cloud/guestbook/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
